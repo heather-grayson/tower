470ce6f296008192e7b7d6599a72f1eece22e97d
Heather Mishon <hmishon92@gmail.com>
2014-01-03 16:55:07 -0500

    dynamic grid working

--- sjs/core/main.js
+++ sjs/core/main.js
@@ -1,13 +1,70 @@
 //= require tower
 
 $(function () {
+	var wrapNum = 1,
+		arraySize = 1,
+		towerArray = [],
+		t;
 
-	var t = new Tower([' ', ' ', ' ', ' ', ' ',
-					   ' ', ' ', ' ', ' ', ' ',
-					   ' ', ' ', ' ', ' ', ' ',
-					   ' ', ' ', ' ', ' ', ' ',
-					   ' ', ' ', ' ', ' ', ' '], 5);
+	// $('#columns').change(function(){
+	// 	createArray(arraySize);
+	// 	// console.log('columns = ' + $('#columns').val() + '');
+	// 	// console.log('array length = ' + arraySize + '');
+	// 	// console.log('wrap = ' + wrapNum + '');	
+	// });
 
-	console.log (t.isStable());
+	// $('#rows').change(function(){
+	// 	arraySize = $('#rows').val() * wrapNum;
+	// 	createArray(arraySize);
+	// 	// console.log('rows = ' + $('#rows').val() + '');
+	// 	// console.log('array length = ' + arraySize + '');
+	// });
+
+	// function createArray(arraylength){
+		
+	// }
+
+	$('#arraySubmit').click(function(){
+		towerArray = [];
+		wrapNum = $('#columns').val();
+		arraySize = $('#rows').val() * wrapNum;
+
+		$('.towerGrid').empty();
+
+		console.log('columns = ' + $('#columns').val() + '');
+		console.log('array length = ' + arraySize + '');
+		console.log('wrap = ' + wrapNum + '');
+
+		console.log(wrapNum);
+		console.log(arraySize);
+
+		for(var i = 0; i < arraySize; i++){
+			towerArray.push(" ");
+			$('.towerGrid').append("<li> </li>");
+		}
+
+		console.log(wrapNum * 50);
+
+		console.log(towerArray);
+
+		$('#gridWrapper').width( (wrapNum * 52) + 'px')
+		t = new Tower( towerArray, wrapNum);
+
+		console.log(t);
+		console.log (t.isStable());
+
+		//buildGrid();
+
+		// if(!t.isStable()){
+		// 	alert('Your tower is NOT stable. Please try again.');
+		// }
+	});
+
+	// function buildGrid(){
+	// 	var buildIndex = 0;
+	// 	for(var i = 0; i < t.length; i++){
+
+	// 	}
+	// }
 
 });
