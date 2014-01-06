c3e92332bfc98a07b8377990250b8f81f2b66fa2
Heather Mishon <hmishon92@gmail.com>
2014-01-06 12:14:51 -0500

    pulling functions out of nested function

--- sjs/core/main.js
+++ sjs/core/main.js
@@ -1,70 +1,184 @@
 //= require tower
 
+var wrapNum = 1,
+	arraySize = 1,
+	towerArray = [],
+	newItem,
+	currIndex,
+	t;
+
 $(function () {
-	var wrapNum = 1,
-		arraySize = 1,
-		towerArray = [],
-		t;
 
-	// $('#columns').change(function(){
-	// 	createArray(arraySize);
-	// 	// console.log('columns = ' + $('#columns').val() + '');
-	// 	// console.log('array length = ' + arraySize + '');
-	// 	// console.log('wrap = ' + wrapNum + '');	
-	// });
+	$('.box').hide();
 
-	// $('#rows').change(function(){
-	// 	arraySize = $('#rows').val() * wrapNum;
-	// 	createArray(arraySize);
-	// 	// console.log('rows = ' + $('#rows').val() + '');
-	// 	// console.log('array length = ' + arraySize + '');
-	// });
-
-	// function createArray(arraylength){
-		
-	// }
 
 	$('#arraySubmit').click(function(){
 		towerArray = [];
 		wrapNum = $('#columns').val();
 		arraySize = $('#rows').val() * wrapNum;
 
+		$('.box').hide();
 		$('.towerGrid').empty();
 
-		console.log('columns = ' + $('#columns').val() + '');
-		console.log('array length = ' + arraySize + '');
-		console.log('wrap = ' + wrapNum + '');
+		// console.log('columns = ' + $('#columns').val() + '');
+		// console.log('array length = ' + arraySize + '');
+		// console.log('wrap = ' + wrapNum + '');
 
-		console.log(wrapNum);
-		console.log(arraySize);
+		// console.log(wrapNum);
+		// console.log(arraySize);
 
 		for(var i = 0; i < arraySize; i++){
 			towerArray.push(" ");
 			$('.towerGrid').append("<li> </li>");
 		}
 
-		console.log(wrapNum * 50);
-
-		console.log(towerArray);
-
-		$('#gridWrapper').width( (wrapNum * 52) + 'px')
+		$('#gridWrapper').width( (wrapNum * 52) + 'px');
 		t = new Tower( towerArray, wrapNum);
+		
+		
+
+		// console.log(wrapNum * 50);
+
+		// console.log(towerArray);
+
+		
 
 		console.log(t);
 		console.log (t.isStable());
 
-		//buildGrid();
-
-		// if(!t.isStable()){
-		// 	alert('Your tower is NOT stable. Please try again.');
+		// stabilityCheck();
+		
+		// function stabilityCheck(index, item){
+		// 	if(!t.isStable()){
+		// 		var blankItem = " ";
+		// 		alert('Your tower is NOT stable. Please try again.');
+		// 		buildingTower(currIndex, blankItem);
+		// 	}else{
+		// 		console.log('it is stable')
+		// 	}
 		// }
+
+		// $('ul.towerGrid li').click(function(e){
+		// 	currIndex = $(this).index(),
+		// 	x = e.clientX,
+		// 	y = e.clientY;
+		// 	console.log(currIndex);
+		// 	// console.log(x);
+		// 	// console.log(y);
+
+		// 	$('.box').css('left', (x - 70) + 'px');
+		// 	$('.box').css('top', y + 'px');
+		// 	$('.box').show();
+
+		// });
+
+
+
+		// $('ul.box li').click(function(e){
+		// 	newItem = $(this).text(),
+		// 	x = e.clientX,
+		// 	y = e.clientY;
+		// 	console.log(newItem);
+		// 	// console.log(x);
+		// 	// console.log(y);
+
+		// 	$('.box').hide();
+
+		// 	buildingTower(currIndex, newItem);
+		// });
+
+
+
+		// $('ul.box li').click(function(e){
+		// 	newItem = $(this).text(),
+		// 	x = e.clientX,
+		// 	y = e.clientY;
+		// 	console.log(newItem);
+		// 	// console.log(x);
+		// 	// console.log(y);
+
+		// 	$('.box').hide();
+
+		// 	buildingTower(currIndex, newItem);
+		// });
+
+		// function buildingTower(index, item){
+		// 	$('.towerGrid').empty();
+		// 	towerArray.splice(index,1,item);
+		// 	t = new Tower(towerArray, wrapNum);
+
+		// 	for(var i = 0; i < arraySize; i++){
+		// 		$('.towerGrid').append("<li>" + towerArray[i] + "</li>");
+		// 	}
+
+		// 	console.log (t.isStable());
+		// 	console.log(towerArray);
+
+		// 	stabilityCheck(index, item);
+		// }
+
+
+
+		$('ul.towerGrid li').unbind().bind('click', showOptions);
+		$('ul.box li').unbind().bind('click', hideOptions);
 	});
-
-	// function buildGrid(){
-	// 	var buildIndex = 0;
-	// 	for(var i = 0; i < t.length; i++){
-
-	// 	}
-	// }
-
+	
 });
+
+function buildingTower(index, item){
+	$('.towerGrid').empty();
+	towerArray.splice(index,1,item);
+	t = new Tower(towerArray, wrapNum);
+
+	for(var i = 0; i < arraySize; i++){
+		$('.towerGrid').append("<li>" + towerArray[i] + "</li>");
+	}
+
+	console.log (t.isStable());
+	console.log(towerArray);
+
+	stabilityCheck(index, item);
+}
+
+function stabilityCheck(index, item){
+	if(!t.isStable()){
+		var blankItem = " ";
+		alert('Your tower is NOT stable. Please try again.');
+		buildingTower(currIndex, blankItem);
+		$('ul.towerGrid li').unbind().bind('click', showOptions);
+		$('ul.box li').unbind().bind('click', hideOptions);
+	}else{
+		console.log('it is stable')
+	}
+}
+
+function showOptions(e){
+	console.log(this);
+	currIndex = $(this).index(),
+	x = e.clientX,
+	y = e.clientY;
+	console.log(currIndex);
+	// console.log(x);
+	// console.log(y);
+
+	$('.box').css('left', (x - 70) + 'px');
+	$('.box').css('top', y + 'px');
+	$('.box').show();
+
+}
+
+function hideOptions(e){
+	newItem = $(this).text();
+	console.log(newItem);
+	// console.log(x);
+	// console.log(y);
+
+	$('.box').hide();
+
+	buildingTower(currIndex, newItem);
+}
+
+
+
+
+
